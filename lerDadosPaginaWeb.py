0c0c637876dfcd344a68c35fe902dedbb1894577
aannddrree <aannddrree.silva@gmail.com>
2019-03-31 21:51:48 -0300

    Implantação

---
+++ lerDadosPaginaWeb.py
@@ -0,0 +1,17 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+from bs4 import BeautifulSoup
+
+try:
+    html = urlopen("https://www.extra.com.br/")
+except HTTPError as e:
+    print(e)
+except URLError:
+    print("Server down or incorrect domain")
+else:
+    res = BeautifulSoup(html.read(),"html5lib")
+    if res.title is None:
+        print("Tag not found")
+    else:
+        print(res.title)
---
+++ lerDadosPaginaWeb2.py
@@ -0,0 +1,36 @@
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup as soup
+
+numeroPagina=1
+
+url = 'https://www.extra.com.br/?Filtro=D36611&Ordenacao=_MaisVendidos&paginaAtual={}&ComparacaoProdutos=&AdicionaListaCasamento='.format(numeroPagina)
+
+req = Request(url , headers={'User-Agent': 'Mozilla/5.0'})
+webpage = urlopen(req).read()
+
+page_soup = soup(webpage, "html.parser")
+
+nomeCelular = page_soup.findAll("strong", "name fn")
+
+listNome = []
+listPreco = []
+
+for container in nomeCelular:
+    descricao = str(container).replace('<strong class="name fn">', '')
+    descricao = descricao.replace('</strong>', '')
+    listNome.append(descricao)
+
+precos = page_soup.findAll("span","for price sale")
+
+for container in precos:
+    preco = str(container).replace('<span class="for price sale">Por: <strong>', '')
+    preco = preco.replace('</strong></span>', '')
+    listPreco.append(preco)
+
+print("DADOS COLETADOS DA PAGINA WEB:")
+print("descricao, preco")
+
+contador = 0
+for dados in nomeCelular:
+    print("{} - {} ".format(str(listNome[contador]),str(listPreco[contador])))
+    contador = contador + 1
